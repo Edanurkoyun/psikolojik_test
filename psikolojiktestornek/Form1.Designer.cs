60dcbaeac7ea41f8109824e7e4046f8e5731572b
Edanurkoyun <148636366+Edanurkoyun@users.noreply.github.com>
2024-05-02 13:51:42 +0300

    Add project files.

---
+++ psikolojiktestornek/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace psikolojiktestornek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Font = new System.Drawing.Font("Rockwell", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(12, 76);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(631, 209);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(12, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(631, 42);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "A)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button2.Location = new System.Drawing.Point(12, 360);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(631, 42);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "B)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button3.Location = new System.Drawing.Point(12, 408);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(631, 42);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "C)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button4.Location = new System.Drawing.Point(12, 456);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(631, 42);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "D)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button5.Location = new System.Drawing.Point(82, 527);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(145, 48);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Başla";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(77, 602);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Toplam Puan:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(230, 602);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "-";
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1152, 512);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 63);
+            this.axWindowsMediaPlayer1.TabIndex = 8;
+            this.axWindowsMediaPlayer1.Visible = false;
+            this.axWindowsMediaPlayer1.Enter += new System.EventHandler(this.axWindowsMediaPlayer1_Enter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(1270, 642);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ psikolojiktestornek/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace psikolojiktestornek
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int soru = 0;
+        int puan = 0;
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Text = "Sonraki";
+            button1.Enabled = true;
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = false;
+            soru++;
+            if(soru==1)
+            {
+                textBox1.Text = "Yeni bir deneyime girerken genellikle ne hissedersiniz?";
+                button1.Text = "Heyecanlanırım";
+                button2.Text = "Endişelenirim";
+                button3.Text = "Merak Ederim";
+                button4.Text = "Kaygılanırım";
+
+            }
+            if(soru==2)
+            {
+                textBox1.Text = "Bir grup insanla etkileşime geçtiğinizde, genellikle hangi rolü üstlenirsiniz?";
+                button1.Text = "Liderlik rolü";
+                button2.Text = "Arka planda kalmayı tercih ederim";
+                button3.Text = "Tartışmaları yönlendiririm";
+                button4.Text = "Diğerlerinin fikirlerini dinlemeyi tercih ederim";
+            }
+            if(soru==3)
+            {
+                textBox1.Text = "Stresli bir durumla karşılaştığınızda, genellikle nasıl tepki verirsiniz?";
+                button1.Text = "Hemen çözüm aramaya başlarım";
+                button2.Text = "Paniklerim";
+                button3.Text = "Durumu değerlendirir ve sakin kalırım";
+                button4.Text = "Diğerlerinden yardım isterim";
+            }
+            if (soru == 4)
+            {
+                textBox1.Text = "Yeni bir projeye başlarken, genellikle ne tür bir yaklaşım benimserdiniz?";
+                button1.Text = "Detaylı bir plan hazırlarım ve adım adım ilerlerim";
+                button2.Text = "Esnek bir plan yaparım ve duruma göre değişiklik yaparım";
+                button3.Text = "Doğrudan eyleme geçerim ve detaylara çok fazla takılmam";
+                button4.Text = "Başkalarının fikirlerini dinler ve onlara göre hareket ederim";
+            }
+            if (soru == 5)
+            {
+                textBox1.Text = "Zor bir karar vermeniz gerektiğinde, genellikle hangi faktörlere öncelik verirsiniz?";
+                button1.Text = "Mantıklı ve akılcı çözümler";
+                button2.Text = "Kişisel duygularım ve içsel hislerim";
+                button3.Text = "Çevremdekilerin fikirleri ve etkileşimler";
+                button4.Text = "Gelecekteki sonuçların değerlendirmesi";
+            }
+            if (soru == 6)
+            {
+                textBox1.Text = "Kendinizi en rahat hissettiğiniz ortam nasıldır?";
+                button1.Text = "Sessiz ve sakin bir ortam";
+                button2.Text = "Canlı ve hareketli bir ortam";
+                button3.Text = "Doğa ile iç içe olan bir ortam";
+                button4.Text = "İnsanlarla dolu ve sosyal bir ortam";
+            }
+            if (soru == 7)
+            {
+                textBox1.Text = "Bir problemle karşılaştığınızda, genellikle nasıl bir yaklaşım izlersiniz?";
+                button1.Text = "Problemi sistematik bir şekilde analiz ederim ve çözüm yolları ararım";
+                button2.Text = "Problemi bir süre unutur ve sonrasında çözüm bulmaya çalışırım";
+                button3.Text = "Yakın çevremdeki kişilerden yardım alırım";
+                button4.Text = "Problemi erteleyerek, doğal bir şekilde çözülmesini beklerim";
+            }
+            if(soru == 8)
+            {
+                textBox1.Text = "Hayatınızda çok önemli olduğunu düşündüğünüz bir hedefiniz var mı?";
+                button1.Text = "Evet, belirli bir hedefim var ve ona ulaşmak için çalışıyorum";
+                button2.Text = "İlgimi çeken birkaç hedefim var, ama net bir odak noktam yok";
+                button3.Text = "Hedef belirlemek yerine, hayatın getirdiği fırsatları değerlendiriyorum";
+                button4.Text = "Hedef koymaktan ziyade, anın tadını çıkarmayı tercih ediyorum";
+
+            }
+            if(soru==9)
+            {
+                textBox1.Text = "Yeni bir konsepti öğrenirken, genellikle hangi öğrenme yöntemini tercih edersiniz?";
+                button1.Text = "Kitaplar ve yazılı kaynaklar aracılığıyla öğrenmeyi tercih ederim";
+                button2.Text = "Pratik yaparak öğrenmeyi tercih ederim";
+                button3.Text = "Görsel materyallerle öğrenmeyi tercih ederim";
+                button4.Text = "Bir öğretmenden birebir öğrenmeyi tercih ederim";
+            }
+            if (soru == 10)
+            {
+                textBox1.Text = "Bir tartışma sırasında, genellikle nasıl bir tavır takınırsınız?";
+                button1.Text = "Fikirlerimi net bir şekilde ifade eder ve savunurum";
+                button2.Text = "Dinlerim ve diğerlerinin fikirlerini anlamaya çalışırım";
+                button3.Text = "Tartışmadan kaçınırım ve konuyu değiştirmeye çalışırım";
+                button4.Text = "Ortamı yumuşatır ve tartışmayı sakinleştirmeye çalışırım";
+            }
+            if (soru == 11)
+            {
+                textBox1.Text = "Testiniz Sona Ermiştir:)";
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = false;
+                button4.Enabled = false;
+                label2.Text=puan.ToString();
+                if(puan>=10 && puan <= 15)
+                {
+                    textBox1.Text = "Bu puan aralığındaki bir kişi genellikle sakin, dengeli ve düşünceli bir yapıya sahip olabilir. İçe dönük olabilir ve genellikle karmaşık durumlarda ve problem çözme süreçlerinde sabırlı ve mantıklı bir yaklaşım sergileyebilir. İletişimde daha içsel ve derin olabilirler.";
+
+                }
+                if(puan>=16 && puan <= 20)
+                {
+                    textBox1.Text = "Bu puan aralığındaki bir kişi, hem içe dönük hem de dışa dönük özellikleri dengeli bir şekilde sergileyebilir. Hem analitik düşünme becerilerine hem de empati yeteneğine sahip olabilirler. İnsanlarla etkileşimde bulunmaktan hoşlanabilir ve grup içinde uyum sağlama yetenekleri gelişmiş olabilir.";
+
+                }
+                if(puan>=21 && puan <= 25)
+                {
+                    textBox1.Text = "Bu puan aralığındaki bir kişi genellikle dışa dönük, enerjik ve liderlik özellikleri sergileyebilir. Kararlılıkla hareket ederken, başkalarının duygularını ve ihtiyaçlarını anlama yeteneğine sahip olabilirler. Grup içinde liderlik rolünü üstlenebilir ve insanları motive etme konusunda etkili olabilirler.";
+
+                }
+                if(puan>=26 && puan <= 30)
+                {
+                    textBox1.Text = "Bu puan aralığındaki bir kişi genellikle kararlı, motive edici ve liderlik vasıflarına sahip olabilir. Olaylara pozitif bir bakış açısına sahip olabilirler ve zorluklarla karşılaştıklarında bile iyimserliklerini koruyabilirler. Kararlılıkları ve enerjileriyle etraflarındaki insanları etkileyebilirler.";
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            puan = puan + 3;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button1.Enabled = false;
+            button5.Enabled = true;
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            puan = puan + 1;
+            button1.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button2.Enabled = false;
+            button5.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            puan=puan + 2;
+            button2.Enabled = false;
+            button1.Enabled = false;
+            button4.Enabled = false;
+            button3.Enabled = false;
+            button5.Enabled = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            puan = puan + 1;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button1.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            axWindowsMediaPlayer1.URL= "C:\\Users\\edanu\\Desktop\\emotional.mp3";
+        }
+
+        private void axWindowsMediaPlayer1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
